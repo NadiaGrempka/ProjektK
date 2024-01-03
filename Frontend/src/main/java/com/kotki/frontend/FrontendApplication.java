ecec1c637f72b5f4f176eec0e1428aa0e1c0b327
Nadia <s27147@pjwstk.edu.pl>
2024-01-03 11:04:33 +0100

    dziala?

---
+++ Frontend/src/main/java/com/kotki/frontend/FrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kotki.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+
+}
